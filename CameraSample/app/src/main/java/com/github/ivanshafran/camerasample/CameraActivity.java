65fa02dd2edc3744d869889d00fb150024cb7438
Ernest Asanov <ernest.asanov@abbyy.com>
2021-04-06 16:16:45 +0300

    [CameraSample] Updated API

--- CameraSample/app/src/main/java/com/github/ivanshafran/camerasample/CameraActivity.java
+++ CameraSample/app/src/main/java/com/github/ivanshafran/camerasample/CameraActivity.java
@@ -11,96 +11,102 @@ import java.io.File;
 import java.util.UUID;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.ImageCapture;
-import androidx.camera.view.CameraView;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.view.CameraController;
+import androidx.camera.view.LifecycleCameraController;
+import androidx.camera.view.PreviewView;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.lifecycle.LifecycleOwner;
 
-public class CameraActivity extends AppCompatActivity implements ImageCapture.OnImageSavedListener {
+public class CameraActivity extends AppCompatActivity implements ImageCapture.OnImageSavedCallback {
 
-    private static final int CAMERA_REQUEST_CODE = 0;
+	private static final int CAMERA_REQUEST_CODE = 0;
 
-    private CameraView cameraView;
-    private View takePictureButton;
+	private LifecycleCameraController cameraController;
+	private PreviewView cameraView;
+	private View takePictureButton;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_camera);
+	@Override
+	protected void onCreate( Bundle savedInstanceState )
+	{
+		super.onCreate( savedInstanceState );
+		setContentView( R.layout.activity_camera );
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
-                == PackageManager.PERMISSION_GRANTED) {
-            startCamera();
-        } else {
-            requestPermission();
-        }
-    }
+		if( ContextCompat.checkSelfPermission( this, Manifest.permission.CAMERA )
+			== PackageManager.PERMISSION_GRANTED ) {
+			startCamera();
+		} else {
+			requestPermission();
+		}
+	}
 
-    private void requestPermission() {
-        ActivityCompat.requestPermissions(
-                this,
-                new String[] {Manifest.permission.CAMERA},
+	private void requestPermission()
+	{
+		ActivityCompat.requestPermissions(
+			this,
+			new String[] { Manifest.permission.CAMERA },
 			CAMERA_REQUEST_CODE
-        );
-    }
+		);
+	}
 
-    @Override
-    public void onRequestPermissionsResult(
-            final int requestCode,
-            @NonNull final String[] permissions,
-            @NonNull final int[] grantResults
-    ) {
-        if (requestCode == CAMERA_REQUEST_CODE ) {
-            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                startCamera();
-            } else {
-                if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
-                    Toast.makeText(this, R.string.need_permission, Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(this, R.string.permission_in_settings, Toast.LENGTH_SHORT).show();
-                }
-                finish();
-            }
-        } else {
-            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-    }
+	@Override
+	public void onRequestPermissionsResult(
+		final int requestCode,
+		@NonNull final String[] permissions,
+		@NonNull final int[] grantResults
+	)
+	{
+		if( requestCode == CAMERA_REQUEST_CODE ) {
+			if( grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED ) {
+				startCamera();
+			} else {
+				if( ActivityCompat.shouldShowRequestPermissionRationale( this, Manifest.permission.CAMERA ) ) {
+					Toast.makeText( this, R.string.need_permission, Toast.LENGTH_SHORT ).show();
+				} else {
+					Toast.makeText( this, R.string.permission_in_settings, Toast.LENGTH_SHORT ).show();
+				}
+				finish();
+			}
+		} else {
+			super.onRequestPermissionsResult( requestCode, permissions, grantResults );
+		}
+	}
 
-    private void startCamera() {
-        cameraView = findViewById(R.id.cameraView);
-        cameraView.setCaptureMode(CameraView.CaptureMode.IMAGE);
-        cameraView.bindToLifecycle((LifecycleOwner) this);
+	private void startCamera()
+	{
+		cameraController = new LifecycleCameraController( this );
+		cameraController.bindToLifecycle( this );
+		cameraController.setEnabledUseCases( CameraController.IMAGE_CAPTURE );
+		cameraView = findViewById( R.id.cameraView );
+		cameraView.setController( cameraController );
 
-        takePictureButton = findViewById(R.id.takePictureButton);
-        takePictureButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(final View v) {
-                cameraView.takePicture(
-                        generatePictureFile(),
-                        AsyncTask.SERIAL_EXECUTOR,
-                        CameraActivity.this
-                );
-            }
-        });
-    }
+		takePictureButton = findViewById( R.id.takePictureButton );
+		takePictureButton.setOnClickListener( v -> {
+			ImageCapture.OutputFileOptions options = new ImageCapture.OutputFileOptions.Builder( generatePictureFile() ).build();
+			cameraController.takePicture(
+				options,
+				AsyncTask.SERIAL_EXECUTOR,
+				CameraActivity.this
+			);
+		} );
+	}
 
-    @Override
-    public void onImageSaved(@NonNull final File file) {
-		int t = 0;
-    }
+	private File generatePictureFile()
+	{
+		return new File( getFilesDir(), UUID.randomUUID().toString() );
+	}
 
-    @Override
-    public void onError(
-            @NonNull final ImageCapture.ImageCaptureError imageCaptureError,
-            @NonNull final String message,
-            @Nullable final Throwable cause) {
-        finish();
-    }
+	@Override
+	public void onImageSaved( @NonNull ImageCapture.OutputFileResults outputFileResults )
+	{
 
-    private File generatePictureFile() {
-        return new File(getFilesDir(), UUID.randomUUID().toString());
-    }
+	}
+
+	@Override
+	public void onError( @NonNull ImageCaptureException exception )
+	{
+		finish();
+	}
 }
