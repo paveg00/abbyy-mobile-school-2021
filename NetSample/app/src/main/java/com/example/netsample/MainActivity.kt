667ad4b96583da25b12ba222b353c3616a86472c
Ernest Asanov <ernest.asanov@abbyy.com>
2021-04-13 15:23:50 +0300

    [NetworkSample] Updated

--- NetSample/app/src/main/java/com/example/netsample/MainActivity.kt
+++ NetSample/app/src/main/java/com/example/netsample/MainActivity.kt
@@ -1,197 +1,178 @@
-package com.example.netsample;
+package com.example.netsample
 
-import android.annotation.SuppressLint;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.TextView;
+import android.annotation.SuppressLint
+import android.os.AsyncTask
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
+import com.google.gson.annotations.SerializedName
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.json.JSONArray
+import org.json.JSONException
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+	private lateinit var textView: TextView
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+		textView = findViewById(R.id.textView)
+		findViewById<View>(R.id.badLoadButton).setOnClickListener(this)
+		findViewById<View>(R.id.loadButton).setOnClickListener(this)
+		findViewById<View>(R.id.loadAsyncButton).setOnClickListener(this)
+		findViewById<View>(R.id.loadVolleyButton).setOnClickListener(this)
+		findViewById<View>(R.id.loadRetrofitButton).setOnClickListener(this)
+	}
 
-import com.google.gson.annotations.SerializedName;
+	override fun onClick(v: View) {
+		when(v.id) {
+			R.id.badLoadButton -> onBadLoadClick()
+			R.id.loadButton -> onLoadClick()
+			R.id.loadAsyncButton -> loadPostAsync()
+			R.id.loadVolleyButton -> loadPostVolley()
+			R.id.loadRetrofitButton -> loadPostRetrofit()
+		}
+	}
 
-import org.json.JSONException;
-import org.json.JSONObject;
+	private fun onBadLoadClick() {
+		showOnTextView(loadPost())
+	}
 
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-import retrofit2.http.GET;
+	@SuppressLint("StaticFieldLeak")
+	private fun onLoadClick() {
+		showOnTextView("")
+		object : AsyncTask<Void?, Void?, String>() {
+			override fun doInBackground(vararg voids: Void?): String {
+				return loadPost()
+			}
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+			override fun onPostExecute(s: String) {
+				showOnTextView(s)
+			}
+		}.execute()
+	}
 
-    private static final String CAT_URL = "https://demo2035120.mockable.io/cat";
-    private static final String DOG_URL = "https://demo2035120.mockable.io/dog";
+	private fun showOnTextView(post: String) {
+		textView.text = post
+	}
 
-    private TextView textView;
+	private fun loadPost(): String {
+		var connection: HttpURLConnection? = null
+		try {
+			val u = URL(POSTS_URL + "posts/")
+			connection = u.openConnection() as HttpURLConnection
+			connection.requestMethod = "GET"
+			connection.connect()
+			val status = connection.responseCode
+			if (status != HttpURLConnection.HTTP_OK) {
+				return "Error"
+			} else {
+				BufferedReader(InputStreamReader(connection.inputStream)).use { reader ->
+					val stringBuilder = StringBuilder()
+					var line: String?
+					while (reader.readLine().also { line = it } != null) {
+						stringBuilder.append(line).append("\n")
+					}
+					return getPostFromJson(JSONArray(stringBuilder.toString()))
+				}
+			}
+		} catch (ex: IOException) {
+			return "Error"
+		} finally {
+			connection?.disconnect()
+		}
+	}
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	private fun loadPostAsync() {
+		showOnTextView("")
+		val okHttpClient = OkHttpClient()
+		// Get по умолчанию
+		val request: Request = Request.Builder().url(POSTS_URL + "posts/").build()
+		// Асинхронность из коробки
+		okHttpClient.newCall(request).enqueue(object : okhttp3.Callback {
+			override fun onFailure(call: okhttp3.Call, e: IOException) {}
 
-        textView = findViewById(R.id.textView);
+			@Throws(IOException::class)
+			override fun onResponse(call: okhttp3.Call, response: okhttp3.Response) {
+				if(response.isSuccessful) {
+					val jsonArray = JSONArray(response.body!!.string())
+					val title = getPostFromJson(jsonArray)
+					runOnUiThread { showOnTextView(title) }
+				}
+			}
+		})
+	}
 
-        findViewById(R.id.badCatButton).setOnClickListener(this);
-        findViewById(R.id.catButton).setOnClickListener(this);
-        findViewById(R.id.dogButton).setOnClickListener(this);
-        findViewById(R.id.horseButton).setOnClickListener(this);
-    }
+	private fun getPostFromJson(jsonArray: JSONArray): String {
+		return try {
+			val jsonObject = jsonArray.getJSONObject(0)
+			jsonObject.getString("title")
+		} catch (e: JSONException) {
+			"Error"
+		}
+	}
 
-    @Override
-    public void onClick(final View v) {
-        switch (v.getId()) {
-            case R.id.badCatButton:
-                onBadCatClick();
-                break;
-            case R.id.catButton:
-                onCatClick();
-                break;
-            case R.id.dogButton:
-                onDogClick();
-                break;
-            case R.id.horseButton:
-                onHorseClick();
-                break;
-            default:
-                // Ничего не делать
-        }
-    }
+	private fun loadPostVolley() {
+		val queue = Volley.newRequestQueue(this)
+		showOnTextView("")
+		val jsonRequest = JsonArrayRequest(com.android.volley.Request.Method.GET, POSTS_URL + "posts/",
+			null,
+			{
+				showOnTextView(getPostFromJson(it))
+			},
+			{
+				showOnTextView("Fail")
+			}
+		)
+		queue.add(jsonRequest)
+	}
 
-    private void onBadCatClick() {
-        showOnTextView(loadCat());
-    }
+	private fun loadPostRetrofit() {
+		val retrofit = Retrofit.Builder()
+			.addConverterFactory(GsonConverterFactory.create())
+			.baseUrl(POSTS_URL)
+			.build()
+		val api = retrofit.create(Api::class.java)
+		val call = api.posts
+		showOnTextView("")
+		call.enqueue(object : Callback<List<Post>> {
+			override fun onResponse(call: Call<List<Post>>, response: Response<List<Post>>) {
+				if (response.isSuccessful) {
+					val body = response.body() ?: return
+					if (body.isEmpty()) return
+					runOnUiThread { showOnTextView(body[0].title) }
+				}
+			}
 
-    @SuppressLint("StaticFieldLeak")
-    private void onCatClick() {
-        new AsyncTask<Void, Void, String>() {
-            @Override
-            protected String doInBackground(final Void... voids) {
-                return loadCat();
-            }
+			override fun onFailure(call: Call<List<Post>>, t: Throwable) {}
+		})
+	}
 
-            @Override
-            protected void onPostExecute(final String s) {
-                showOnTextView(s);
-            }
-        }.execute();
-    }
+	internal class Post(
+		@SerializedName("title")
+		val title: String
+	)
 
-    private void onDogClick() {
-        loadDogAsync();
-    }
+	internal interface Api {
+		@get:GET("posts")
+		val posts: Call<List<Post>>
+	}
 
-    private void onHorseClick() {
-        loadHorseAsync();
-    }
-
-    private void showOnTextView(@NonNull final String animal) {
-        textView.setText(String.format("%s loaded", animal));
-    }
-
-    private String loadCat() {
-        HttpURLConnection connection = null;
-        try {
-            URL u = new URL(CAT_URL);
-            connection = (HttpURLConnection) u.openConnection();
-            connection.setRequestMethod("GET");
-            connection.connect();
-
-            int status = connection.getResponseCode();
-            if (status != HttpURLConnection.HTTP_OK) {
-                return "Error";
-            } else {
-                try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-                    StringBuilder stringBuilder = new StringBuilder();
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        stringBuilder.append(line + "\n");
-                    }
-                    return parseAnimalFromJson(stringBuilder.toString());
-                }
-            }
-        } catch (IOException ex) {
-            return "Error";
-        } finally {
-            if (connection != null) {
-                connection.disconnect();
-            }
-        }
-    }
-
-    private void loadDogAsync() {
-        OkHttpClient okHttpClient = new OkHttpClient();
-        // Get по умолчанию
-        Request request = new Request.Builder().url(DOG_URL).build();
-        // Асинхронность их коробки
-        okHttpClient.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(final Call call, final IOException e) {
-
-            }
-
-            @Override
-            public void onResponse(final Call call, final Response response) throws IOException {
-                if (response.isSuccessful()) {
-                    final String animal = parseAnimalFromJson(response.body().string());
-                    runOnUiThread(() -> showOnTextView(animal));
-                }
-            }
-        });
-    }
-
-    private String parseAnimalFromJson(@NonNull final String json) {
-        try {
-            JSONObject jsonObject = new JSONObject(json);
-            return jsonObject.getString("animal");
-        } catch (JSONException e) {
-            return "Error";
-        }
-    }
-
-    private void loadHorseAsync() {
-        Retrofit retrofit = new Retrofit
-                .Builder()
-                .addConverterFactory(GsonConverterFactory.create())
-                .baseUrl("https://demo2035120.mockable.io")
-                .build();
-        Api api = retrofit.create(Api.class);
-
-        retrofit2.Call<Animal> call = api.getAnimal();
-        call.enqueue(new retrofit2.Callback<Animal>() {
-            @Override
-            public void onResponse(final retrofit2.Call<Animal> call, final retrofit2.Response<Animal> response) {
-                if (response.isSuccessful()) {
-                    runOnUiThread(() -> showOnTextView(response.body().name));
-                }
-            }
-
-            @Override
-            public void onFailure(final retrofit2.Call<Animal> call, final Throwable t) {
-
-            }
-        });
-    }
-
-    class Animal {
-
-        @SerializedName("animal")
-        public String name;
-    }
-
-    interface Api {
-
-        @GET("horse")
-        retrofit2.Call<Animal> getAnimal();
-    }
+	companion object {
+		private const val POSTS_URL = "https://jsonplaceholder.typicode.com/"
+	}
 }
